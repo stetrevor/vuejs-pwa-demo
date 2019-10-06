544fd80f1979b26b323ae79aab0c04d77a08a5c7
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-06 11:44:24 +0800

    feat: Register sw using Workbox

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,32 +1,17 @@
-/* eslint-disable no-console */
+import { Workbox } from "workbox-window";
 
-import { register } from 'register-service-worker'
+let wb;
 
-if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
-    },
-    registered () {
-      console.log('Service worker has been registered.')
-    },
-    cached () {
-      console.log('Content has been cached for offline use.')
-    },
-    updatefound () {
-      console.log('New content is downloading.')
-    },
-    updated () {
-      console.log('New content is available; please refresh.')
-    },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
-    },
-    error (error) {
-      console.error('Error during service worker registration:', error)
-    }
-  })
+if ("serviceWorker" in navigator) {
+  wb = new Workbox(`${process.env.BASE_URL}service-worker.js`);
+
+  wb.addEventListener("controlling", () => {
+    window.location.reload();
+  });
+
+  wb.register();
+} else {
+  wb = null;
 }
+
+export default wb;
