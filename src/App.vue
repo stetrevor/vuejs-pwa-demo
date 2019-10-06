0c5929fd21374d88a829bdf2a220d37ef1b72aea
Trevor Wong <stephen.trevor.wong@outlook.com>
2019-10-06 12:14:18 +0800

    feat: Add upgrade dialog to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,57 @@
 <template>
   <div id="app">
+    <h1>App version: v1.1.0</h1>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
+
+    <div class="upgrade-dialog" v-if="prompt">
+      <div class="upgrade-dialog__message">
+        A new version is found. Refresh to load it?
+      </div>
+      <div
+        class="upgrade-dialog__button upgrade-dialog__button--accept"
+        @click="upgrade"
+      >
+        Refresh
+      </div>
+      <div
+        class="upgrade-dialog__button upgrade-dialog__button--cancel"
+        @click="prompt = false"
+      >
+        Later
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    if (this.$workbox) {
+      this.$workbox.addEventListener("waiting", () => {
+        this.prompt = true;
+      });
+    }
+  },
+
+  methods: {
+    async upgrade() {
+      this.prompt = false;
+
+      await this.$workbox.messageSW({ type: "SKIP_WAITING" });
+    }
+  },
+
+  data() {
+    return {
+      prompt: false
+    };
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
@@ -23,6 +68,39 @@
     color: #2c3e50;
     &.router-link-exact-active {
       color: #42b983;
+    }
+  }
+}
+
+.upgrade-dialog {
+  position: fixed;
+  bottom: 24px;
+  left: 50%;
+  transform: translateX(-50%);
+  border-radius: 4px;
+  box-sizing: border-box;
+  padding: 8px 12px;
+  color: white;
+  background-color: #2c3e50;
+  box-shadow: 0 4px 4px rgba(#2c3e50, 0.4);
+  display: grid;
+  grid-template-columns: 1fr max-content;
+  gap: 8px;
+
+  &__message {
+    grid-column: 1 / 3;
+  }
+
+  &__button {
+    padding: 0 8px;
+    height: 36px;
+    line-height: 36px;
+    font-weight: bold;
+    cursor: pointer;
+    user-select: none;
+
+    &--accept {
+      justify-self: end;
     }
   }
 }
