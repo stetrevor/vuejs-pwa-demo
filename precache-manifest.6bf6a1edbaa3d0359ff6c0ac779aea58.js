25414eac487e4d9e2c81318f0d819cee8e80434c
Trevor Wong <stephen.trevor.wong@outlook.com>
2020-05-25 18:36:48 +0800

    deploy

---
+++ precache-manifest.6bf6a1edbaa3d0359ff6c0ac779aea58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a1a3092a04a1905491c",
+    "url": "css/app.73250357.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a8cd23871889353d13f8badd62d52bcc",
+    "url": "index.html"
+  },
+  {
+    "revision": "5a1a3092a04a1905491c",
+    "url": "js/app.ad60e9e6.js"
+  },
+  {
+    "revision": "9442ce73c13e21e8e928",
+    "url": "js/chunk-vendors.a3fd750e.js"
+  },
+  {
+    "revision": "3868a87186592ee3caa21fc69400dc7f",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
