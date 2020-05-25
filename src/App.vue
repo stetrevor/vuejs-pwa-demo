2a6d51cda3d374668180b82f83f3385accd15128
Trevor Wong <stephen.trevor.wong@outlook.com>
2020-05-25 16:34:15 +0800

    Update dialog with service worker

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Vue.js with PWA" />
+
+    <div class="update-dialog" v-if="prompt">
+      <div class="update-dialog__content">
+        A new version is found. Refresh to load it?
+      </div>
+      <div class="update-dialog__actions">
+        <button
+          class="update-dialog__button update-dialog__button--confirm"
+          @click="update"
+        >
+          Update
+        </button>
+        <button
+          class="update-dialog__button update-dialog__button--cancel"
+          @click="prompt = false"
+        >
+          Cancel
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +32,27 @@ export default {
   name: "App",
   components: {
     HelloWorld
+  },
+
+  methods: {
+    async update() {
+      this.prompt = false;
+      await this.$workbox.messageSW({ type: "SKIP_WAITING" });
+    }
+  },
+
+  data() {
+    return {
+      prompt: false
+    };
+  },
+
+  async created() {
+    if (this.$workbox) {
+      this.$workbox.addEventListener("waiting", () => {
+        this.prompt = true;
+      });
+    }
   }
 };
 </script>
@@ -24,5 +65,32 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.update-dialog {
+  position: fixed;
+  left: 50%;
+  bottom: 64px;
+  transform: translateX(-50%);
+  border-radius: 4px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+  padding: 12px;
+  max-width: 576px;
+  color: white;
+  background-color: #2c3e50;
+  text-align: left;
+
+  &__actions {
+    display: flex;
+    margin-top: 8px;
+  }
+
+  &__button {
+    margin-right: 8px;
+
+    &--confirm {
+      margin-left: auto;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
+import wb from "./registerServiceWorker";
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$workbox = wb;
 
 new Vue({
   render: h => h(App)
